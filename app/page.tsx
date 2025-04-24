1d1dd6f262d22d1391f8805890f3fa55a689a1d6
Rashmi <kr040902@gmail.com>
2025-04-24 14:35:44 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,159 @@
-import Image from "next/image";
+"use client";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [turn, setTurn] = useState("x");
+  const [count, setCount] = useState(0);
+  const [values, setValues] = useState<string[][]>(
+    Array(3)
+      .fill(null)
+      .map(() => Array(3).fill(""))
+  );
+  useEffect(() => {
+    checkWinner();
+  }, [values]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const handleChangeValue = (x: number, y: number) => {
+    if (values[x][y] == "x" || values[x][y] == "o") return;
+    const newValues = values.map((row) => [...row]);
+    newValues[x][y] = turn;
+
+    setValues(newValues);
+    const newCount = count + 1;
+    setCount(newCount);
+    turn == "x" ? setTurn("o") : setTurn("x");
+    console.log(count);
+  };
+  const handleNewGame = () => {
+    setValues(
+      Array(3)
+        .fill(null)
+        .map(() => Array(3).fill(""))
+    );
+    setCount(0);
+  };
+
+  const isWinningLine = (a: string, b: string, c: string) => {
+    if (a !== "" && a === b && b === c) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const checkWinner = () => {
+    //TODO check winner and if it does not exist then move to the next step
+    //Row check
+    for (let i = 0; i < 3; i++) {
+      if (isWinningLine(values[i][0], values[i][1], values[i][2])) {
+        declareWinner(values[i][0]);
+        return;
+      }
+    }
+
+    //column check
+    for (let i = 0; i < 3; i++) {
+      if (isWinningLine(values[0][i], values[1][i], values[2][i])) {
+        declareWinner(values[0][i]);
+        return;
+      }
+    }
+
+    //diagonal check
+
+    if (isWinningLine(values[0][0], values[1][1], values[2][2])) {
+      declareWinner(values[0][0]);
+      return;
+    }
+
+    if (isWinningLine(values[0][2], values[1][1], values[2][0])) {
+      declareWinner(values[0][2]);
+      return;
+    }
+
+    if (count == 8) {
+      console.log("game completed");
+    }
+  };
+
+  const declareWinner = (winner: string) => {
+    alert(`${winner} Wins!`);
+    setTimeout(handleNewGame, 1500);
+  };
+
+  return (
+    <>
+      <div className="flex justify-center items-center h-[100vh]">
+        <div className="h-150 w-150 outline-solid  grid grid-cols-3">
+          {values.map((row, i) =>
+            row.map((cell, j) => (
+              <div
+                key={`${i}-${j}`}
+                className="h-50 w-50 outline-solid text-9xl flex justify-center items-center hover:bg-emerald-700 cursor-pointer"
+                onClick={() => handleChangeValue(i, j)}
+              >
+                {cell}
+              </div>
+            ))
+          )}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        {/* <div className="h-150 w-150 outline-solid  grid grid-cols-3">
+          <div
+            className="h-50 w-50 outline-solid text-9xl flex justify-center items-center hover:bg-emerald-700 cursor-pointer"
+            onClick={() => handleChangeValue(0, 0)}
+          >
+            {values[0][0]}
+          </div>
+          <div
+            className="h-50 w-50 outline-solid text-9xl flex justify-center items-center hover:bg-emerald-700 cursor-pointer"
+            onClick={() => handleChangeValue(0, 1)}
+          >
+            {values[0][1]}
+          </div>
+          <div
+            className="h-50 w-50 outline-solid text-9xl flex justify-center items-center hover:bg-emerald-700 cursor-pointer"
+            onClick={() => handleChangeValue(0, 2)}
+          >
+            {values[0][2]}
+          </div>
+          <div
+            className="h-50 w-50 outline-solid text-9xl flex justify-center items-center hover:bg-emerald-700 cursor-pointer"
+            onClick={() => handleChangeValue(1, 0)}
+          >
+            {values[1][0]}
+          </div>
+          <div
+            className="h-50 w-50 outline-solid text-9xl flex justify-center items-center hover:bg-emerald-700 cursor-pointer"
+            onClick={() => handleChangeValue(1, 1)}
+          >
+            {values[1][1]}
+          </div>
+          <div
+            className="h-50 w-50 outline-solid text-9xl flex justify-center items-center hover:bg-emerald-700 cursor-pointer"
+            onClick={() => handleChangeValue(1, 2)}
+          >
+            {values[1][2]}
+          </div>
+          <div
+            className="h-50 w-50 outline-solid text-9xl flex justify-center items-center hover:bg-emerald-700 cursor-pointer"
+            onClick={() => handleChangeValue(2, 0)}
+          >
+            {values[2][0]}
+          </div>
+          <div
+            className="h-50 w-50 outline-solid text-9xl flex justify-center items-center hover:bg-emerald-700 cursor-pointer"
+            onClick={() => handleChangeValue(2, 1)}
+          >
+            {values[2][1]}
+          </div>
+          <div
+            className="h-50 w-50 outline-solid text-9xl flex justify-center items-center hover:bg-emerald-700 cursor-pointer"
+            onClick={() => handleChangeValue(2, 2)}
+          >
+            {values[2][2]}
+          </div>
+        </div>  */}
+      </div>
+    </>
   );
 }
